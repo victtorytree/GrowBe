782fe98877bbc9092da77b96702835eda11c5ccb
victorytree <115655076+victtorytree@users.noreply.github.com>
2023-05-15 09:58:16 +0300

    Update script_3.js

--- js/script_3.js
+++ js/script_3.js
@@ -13,6 +13,7 @@ for (let i = 0; i < days.length; i++) {
   });
 };
 
+
 // Week switch
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
 let date = new Date();
@@ -27,7 +28,6 @@ for (let i = 0; i < 7; i++) {
     break;
   }
 };
-
 function print(inValue) {
   let beginMonth = months[new Date(year, month, inValue).getMonth()];
   let endMonth = months[new Date(year, month, inValue + 6).getMonth()];
@@ -53,28 +53,6 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
-// Show new blocks in .info <div>
-  const trialEl = document.querySelector(".tutor__trial");
-  const videoEl = document.querySelector(".tutor__intro");
-  const trialElCopy = trialEl.cloneNode(true);
-  trialElCopy.classList.add("tutor__trial_small");
-  const socialIcons = document.querySelector(".bottom-icons").cloneNode(true);
-  trialElCopy.append(socialIcons);
-  const videoElCopy = videoEl.cloneNode(true);
-  videoElCopy.classList.add("tutor__video_small");
-  const targetEl = document.querySelector("#main-block");
-
-  window.onresize = function() {
-    if (window.innerWidth < 992) {
-      targetEl.prepend(videoElCopy);
-      targetEl.appendChild(trialElCopy);
-    }
-      else {
-        trialElCopy.remove();
-        videoElCopy.remove();
-      }
-  };
-  
 // Select for resume on mobile
   const tutorArr = ['Sunderland BSc Hons', ['Philadelphia College', ' Tutorify'], 'Art degree'];
   const yearArray = ['2010-2015', ['2015-2017', ' 2016-current'], '2017'];
