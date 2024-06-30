2e8c5ccb224d55ae6157639d65fb6d24b21edacc
vica <vicarom806@gmail.com>
2024-06-30 12:48:19 +0300

    editing June 2024

---
+++ js/script.js
@@ -0,0 +1,47 @@
+
+
+$(document).ready(function(){
+    $( ".menu-button" ).click(function() {
+    $('.navigation__list').toggleClass( "menu_active" );
+  });
+
+  $(".s-clients__row").owlCarousel( {
+    loop: true,
+    smartSpeed: 700,
+    dots: true,
+    dotsEach: true,
+    startPosition: 1,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:3.2
+        }
+    },
+
+  });
+
+  $(".s-news__row").owlCarousel( {
+    loop: true,
+    smartSpeed: 700,
+    dots: true,
+    dotsEach: true,
+    startPosition: 1,
+    responsive:{
+        0:{
+            items:1
+        },
+        900:{
+            items:2
+        },
+        1200:{
+            items:2.4
+        }
+    },
+
+  });
+});
