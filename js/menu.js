f3bee3ed5559311e2bae9e7480b1b742731a74a1
vica <vicarom806@gmail.com>
2024-06-30 14:51:25 +0300

    commit June 2024

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,5 @@
-// Menu
-  (function () {
-    const menu = document.querySelector('.menu-button');
-    menu.addEventListener('click', () => {
-      document.querySelector('.navigation__list').classList.toggle('menu_active');
-    })
-  });
+
+const menu = document.querySelector('.menu-button');
+menu.addEventListener('click', () => {
+  document.querySelector('.nav-t__list').classList.toggle('menu_active');
+});
--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-
-
 $(document).ready(function(){
     $( ".menu-button" ).click(function() {
-    $('.navigation__list').toggleClass( "menu_active" );
+    $('.nav-t__list').toggleClass( "menu_active" );
   });
 
   $(".s-clients__row").owlCarousel( {
@@ -46,51 +43,3 @@ $(document).ready(function(){
 
   });
 });
-=======
-$(document).ready(function(){
-    // Menu
-  $( ".menu-button" ).click(function() {
-  $('.navigation__list').toggleClass( "menu_active" );
-    });
-    
-    // Carousel
-  $(".s-clients__row").owlCarousel( {
-    loop: true,
-    smartSpeed: 700,
-    dots: true,
-    dotsEach: true,
-    startPosition: 1,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        1000:{
-            items:3.2
-        }
-    },
-    autoHeight: true
-  });
-  $(".s-news__row").owlCarousel( {
-    loop: true,
-    smartSpeed: 700,
-    dots: true,
-    dotsEach: true,
-    startPosition: 1,
-    responsive:{
-        0:{
-            items:1
-        },
-        900:{
-            items:2
-        },
-        1200:{
-            items:2.4
-        }
-    },
-  });
-
-});
->>>>>>> 91d3b951844952f9063601ae28a3ad3e3a1883d2
